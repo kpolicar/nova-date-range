d3ee297b465044c4f385d298d78b675b7ded137d
Ahmet Ozisik <ozisikahmet@gmail.com>
2021-07-09 13:03:14 +0300

    fix: add firstDayOfWeek prop

--- resources/js/components/DateRangePicker.vue
+++ resources/js/components/DateRangePicker.vue
@@ -27,7 +27,8 @@ export default {
         seperator: {
             type: String,
             default: '-',
-        }
+        },
+        firstDayOfWeek: { type: Number, default: 0 },
     },
 
     data: () => ({ flatpickr: null }),
